1f34696dfebe549ad9fa55946ee204fb7a653889
Gilbert Tan <57788135+gbtan1991@users.noreply.github.com>
2024-01-11 11:51:13 +0800

    Add the styling in the social links

--- src/component/SocialLinks.jsx
+++ src/component/SocialLinks.jsx
@@ -1,51 +1,65 @@
-import React from 'react'
-import { FaGithub, FaHashnode, FaCodepen } from 'react-icons/fa6'
+import React from "react";
+import { FaGithub, FaHashnode, FaCodepen } from "react-icons/fa6";
+import { SiBuymeacoffee } from "react-icons/si";
 
 const SocialLinks = () => {
-
+  
   const codingProfiles = [
     {
-        id: 1,
-        name: 'GitHub',
-        link: 'https://github.com/your-username',
-        icon: <FaGithub />
+      id: 1,
+      name: "GitHub",
+      link: "https://github.com/gbtan1991",
+      color: "#22272e",
+      icon: <FaGithub />,
     },
     {
-        id: 2,
-        name: 'Hashnode',
-        link: 'https://hashnode.com/@your-username',
-        icon: <FaHashnode />
+      id: 2,
+      name: "Hashnode",
+      link: "https://hashnode.com/@gbtan1991",
+      color: "#2760fd",
+      icon: <FaHashnode />,
     },
     {
-        id: 3,
-        name: 'CodePen',
-        link: 'https://codepen.io/your-username',
-        icon: <FaCodepen />
-    }
-  ]
-
+      id: 3,
+      name: "CodePen",
+      link: "https://codepen.io/gbtan1991",
+      color: "#63d7d0",
+      icon: <FaCodepen />,
+    },
+    {
+      id: 4,
+      name: "BuyMeACoffee",
+      link: "https://www.buymeacoffee.com/gbtan1991",
+      color: "#ffdd00",
+      icon: <SiBuymeacoffee />,
+    },
+  ];
+  const hoverColor = codingProfiles.color
+  
 
   return (
-    <div className='w-full h-2/4 bg-zinc-50'>
-    <ul className='flex justify-center items-center h-full'>
-      {codingProfiles.map((profile) => (
-        <li key={profile.id} className='mx-4 border'>
-          <a
-            href={profile.link}
-            target='_blank'
-            rel='noopener noreferrer'
-            className='flex flex-col items-center'
-          >
-            {profile.icon}
-            <span className='mt-2'>{profile.name}</span>
-          </a>
-        </li>
-      ))}
-    </ul>
-  </div>
-  )
-}
+    <div className="w-full h-2/4 bg-zinc-50">
+      <div className="grid place-content-center mt-12">
+        <ul className="flex flex-col justify-center gap-5">
+        {codingProfiles.map((profile) => (
+          <li key={profile.id} className=
+          {`flex hover:bg-[${profile.color}] hover:transform transition-transform duration-100 ease-in-out hover:scale-150 hover:ease-linear`}>
+            <a href={profile.link} target="_blank" className={`flex flex-row items-center gap-5 text-xl py-5 px-14 w-full rounded-full`}>              
+              <span >{profile.icon}</span>
+              <p className="">{profile.name}</p>
+              {profile.color}
+            </a>
+          </li>
+        ))}
 
-export default SocialLinks
+        </ul>
+
+      </div>
 
 
+
+    </div>
+  );
+};
+
+export default SocialLinks;
