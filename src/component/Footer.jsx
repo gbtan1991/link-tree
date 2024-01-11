44adb874a5bce7e3631293504cdcd7181843c4e9
Gilbert Tan <57788135+gbtan1991@users.noreply.github.com>
2024-01-11 14:00:51 +0800

    Fixed the footer

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     const year = new Date().getFullYear();
   return (
-    <div className='bg-zinc-500 h-20 w-11/12 fixed bottom-0 rounded-br-[44px] rounded-bl-[44px]'>
+    <div className='bg-zinc-500 h-20 w-11/12 sm:w-[375px] fixed bottom-0 rounded-br-[44px] rounded-bl-[44px]'>
       <div className='flex justify-center items-center h-full'>
         <p className='text-white text-base font-bold'>Copyright © {year}. All rights reserved.</p>
     </div>
